753da825ef9b1a462605b76aa7638f0e41c0883c
daniel <daniel@daniel-pc>
2024-07-04 15:15:53 -0400

    crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CarnetsModule } from './carnets/carnets.module';
+import { DepartmentModule } from './department/department.module';
+import { StateModule } from './state/state.module';
+import { StatusesModule } from './statuses/statuses.module';
+import { MunicipalitiesModule } from './municipalities/municipalities.module';
+import { ParishesModule } from './parishes/parishes.module';
+import { ChargeModule } from './charge/charge.module';
 
 @Module({
-  imports: [AuthModule, CarnetsModule],
+  imports: [AuthModule, CarnetsModule, DepartmentModule, StateModule, StatusesModule, MunicipalitiesModule, ParishesModule, ChargeModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/parishes/dto/update-parish.dto.ts
@@ -0,0 +1,4 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateParishDto } from './create-parish.dto';
+
+export class UpdateParishDto extends PartialType(CreateParishDto) {}
